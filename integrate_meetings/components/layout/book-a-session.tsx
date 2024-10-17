844ac17dda8432d6d9d58fc0ca9a03ea0e9ebb3f
JubairShaik <jubairahmed3000@gmail.com>
2024-10-17 17:07:26 +0530

    Charan Enjoy als0 Manoj

--- integrate_meetings/components/layout/book-a-session.tsx
+++
@@ -1,9 +0,0 @@
-import { BookingForm } from "./booking-form";
-
-export const BookASession = () => {
-    return (
-        <div className="max-w-screen-lg mx-auto space-y-12 p-6">
-            <BookingForm />
-        </div>
-    );
-};
